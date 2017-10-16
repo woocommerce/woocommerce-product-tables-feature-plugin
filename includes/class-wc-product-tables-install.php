7ccaa892bc0d2a38da372e7fa29d80bf14973f1a
Gerhard Potgieter <potgieterg@gmail.com>
2017-10-16 14:15:21 -0700

    Basic install class structure

---
+++ includes/class-wc-product-tables-install.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Installation related functions and actions
+ *
+ * @author Automattic
+ **/
+
+class WC_Product_Tables_Install {
+
+	/**
+	 * Constructor
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+
+	/**
+	 * Activate function, runs on plugin activation
+	 *
+	 * @return void
+	 */
+	public function activate() {
+
+	}
+}
