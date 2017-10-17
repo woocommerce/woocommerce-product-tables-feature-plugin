2b4a19f80de4802397bf91ada6319c45f77669bb
Claudio Sanches <contato@claudiosanches.com>
2017-10-17 15:40:13 -0200

    Added initial data stores

---
+++ includes/data-stores/class-wc-product-grouped-data-store-custom-table.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WC Grouped Product Data Store: Stored in Custom Table
+ *
+ * @author   Automattic
+ * @category Data_Store
+ * @package  WooCommerce/Classes/Data_Store
+ * @version  1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Grouped Product Data Store class.
+ */
+class WC_Product_Grouped_Data_Store_Custom_Table extends WC_Product_Grouped_Data_Store_CPT implements WC_Object_Data_Store_Interface {
+
+}
