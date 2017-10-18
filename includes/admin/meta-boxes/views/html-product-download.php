3b43b5eb9cc568d6e745b98a49feffb35711ed88
Claudio Sanches <contato@claudiosanches.com>
2017-10-18 17:07:16 -0200

    Fixed wrong download ID

--- includes/admin/meta-boxes/views/html-product-download.php
+++ includes/admin/meta-boxes/views/html-product-download.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <tr>
 	<td class="sort"></td>
 	<td class="file_name">
-		<input type="hidden" name="_wc_table_file_ids[]" value="<?php echo esc_attr( isset( $file['expiry'] ) ? $file['expiry'] : '0' ); ?>" />
+		<input type="hidden" name="_wc_table_file_ids[]" value="<?php echo esc_attr( isset( $file['id'] ) ? $file['id'] : '0' ); ?>" />
 		<input type="text" class="input_text" placeholder="<?php esc_attr_e( 'File name', 'woocommerce' ); ?>" name="_wc_table_file_names[]" value="<?php echo esc_attr( $file['name'] ); ?>" />
 	</td>
 	<td class="file_url"><input type="text" class="input_text" placeholder="<?php esc_attr_e( 'http://', 'woocommerce' ); ?>" name="_wc_table_file_urls[]" value="<?php echo esc_attr( $file['file'] ); ?>" /></td>
