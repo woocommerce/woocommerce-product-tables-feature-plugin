f2ab864d1076de22c124daa3d28867e983a4d8f6
Claudio Sanches <contato@claudiosanches.com>
2018-03-29 14:39:10 -0300

    Fixed value displayed in the admin

--- includes/admin/meta-boxes/views/html-product-download.php
+++ includes/admin/meta-boxes/views/html-product-download.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</td>
 	<td class="file_url"><input type="text" class="input_text" placeholder="<?php esc_attr_e( 'http://', 'woocommerce' ); ?>" name="_wc_table_file_urls[]" value="<?php echo esc_attr( $file['file'] ); ?>" /></td>
 	<td class="file_url_choose" width="1%"><a href="#" class="button upload_file_button" data-choose="<?php esc_attr_e( 'Choose file', 'woocommerce' ); ?>" data-update="<?php esc_attr_e( 'Insert file URL', 'woocommerce' ); ?>"><?php echo esc_html( str_replace( ' ', '&nbsp;', __( 'Choose file', 'woocommerce' ) ) ); ?></a></td>
-	<td class="file-limit"><input type="number" step="1" min="0" class="input_text" placeholder="<?php esc_attr_e( 'Unlimited', 'woocommerce' ); ?>" name="_wc_table_file_limits[]" value="<?php echo esc_attr( isset( $file['limit'] ) ? $file['limit'] : '' ); ?>" /></td>
-	<td class="file-expiry"><input type="number" step="1" min="0" class="input_text" placeholder="<?php esc_attr_e( 'Never', 'woocommerce' ); ?>" name="_wc_table_file_expiries[]" value="<?php echo esc_attr( isset( $file['expiry'] ) ? $file['expiry'] : '' ); ?>" /></td>
+	<td class="file-limit"><input type="number" step="1" min="0" class="input_text" placeholder="<?php esc_attr_e( 'Unlimited', 'woocommerce' ); ?>" name="_wc_table_file_limits[]" value="<?php echo esc_attr( isset( $file['limit'] ) && -1 < $file['limit'] ? $file['limit'] : '' ); ?>" /></td>
+	<td class="file-expiry"><input type="number" step="1" min="0" class="input_text" placeholder="<?php esc_attr_e( 'Never', 'woocommerce' ); ?>" name="_wc_table_file_expiries[]" value="<?php echo esc_attr( isset( $file['expiry'] ) && -1 < $file['expiry'] ? $file['expiry'] : '' ); ?>" /></td>
 	<td><a href="#" class="delete"><?php esc_html_e( 'Delete', 'woocommerce' ); ?></a></td>
 </tr>
