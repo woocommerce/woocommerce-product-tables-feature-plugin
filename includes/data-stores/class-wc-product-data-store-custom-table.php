663ad128f2547336cf2b44330ee98462b25069b8
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-04-26 17:00:32 -0300

    Update inline documentation

--- includes/data-stores/class-wc-product-data-store-custom-table.php
+++ includes/data-stores/class-wc-product-data-store-custom-table.php
@@ -1193,7 +1193,7 @@ class WC_Product_Data_Store_Custom_Table extends WC_Data_Store_WP implements WC_
 		if ( $existing_downloads ) {
 			$downloads = array();
 			foreach ( $existing_downloads as $data ) {
-				// @todo Should delete downloads that does not have any name or file?
+				// @todo Should delete downloads that does not have any file?
 				if ( empty( $data->file ) ) {
 					continue;
 				}
