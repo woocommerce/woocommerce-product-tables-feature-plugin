91c9119ccc4c2aff3c63c071ac8fbf1227b9dd69
Mike Jolley <mike.jolley@me.com>
2019-01-28 10:32:07 +0000

    More efficient cache priming

--- includes/data-stores/class-wc-product-variation-data-store-custom-table.php
+++ includes/data-stores/class-wc-product-variation-data-store-custom-table.php
@@ -577,22 +577,20 @@ class WC_Product_Variation_Data_Store_Custom_Table extends WC_Product_Data_Store
 	protected function get_parent_product_attribute_names( &$product ) {
 		global $wpdb;
 
-		$attributes = wp_cache_get( 'woocommerce_parent_product_attribute_names_' . $product->get_parent_id(), 'product' );
+		$attributes = wp_cache_get( 'woocommerce_product_attributes_' . $product->get_parent_id(), 'product' );
 
 		if ( false === $attributes ) {
-			$attributes = wp_list_pluck(
-				$wpdb->get_results(
-					$wpdb->prepare(
-						"SELECT `product_attribute_id`, `name` FROM {$wpdb->prefix}wc_product_attributes WHERE product_id = %d",
-						$product->get_parent_id()
-					)
-				), 'name', 'product_attribute_id'
+			$attributes = $wpdb->get_results(
+				$wpdb->prepare(
+					"SELECT * FROM {$wpdb->prefix}wc_product_attributes WHERE product_id = %d",
+					$product->get_parent_id()
+				)
 			); // WPCS: db call ok, cache ok.
 
-			wp_cache_set( 'woocommerce_parent_product_attribute_names_' . $product->get_parent_id(), $attributes, 'product' );
+			wp_cache_set( 'woocommerce_product_attributes_' . $product->get_parent_id(), $attributes, 'product' );
 		}
 
-		return $attributes;
+		return wp_list_pluck( $attributes, 'name', 'product_attribute_id' );
 	}
 
 	/**
